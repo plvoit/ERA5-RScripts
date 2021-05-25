7c6332e2d01422f3df9ca15da088713c3c2f65ff
pittyplaetsch <pittyplaetsch@gmail.com>
2021-05-25 11:54:43 +0200

    sf

--- TimeseriesAnalysis.R
+++ TimeseriesAnalysis.R
@@ -13,17 +13,25 @@ CR$Date <- as.POSIXct(CR$Date)
 CR[,2:ncol(CR)] <- lapply(CR[,2:ncol(CR)], function(x) as.numeric(gsub(" ","",x)))
 
 
-plot(ATHN$ATHN_100_t~ATHN$Date, type = "l")
-plot(CR$ATHN~CR$Date, type = "l")
+#plot(ATHN$ATHN_100_t~ATHN$Date, type = "l")
+#plot(CR$ATHN~CR$Date, type = "l")
 
 #add extra line (copy first line) for aggregation starting at 00:00
 ATHN <- rbind(ATHN[1,],ATHN)
 ATHN[1,1] <- as.POSIXct("2011-01-01 00:00:00")
+ATHN$Date <- as.POSIXct(ATHN$Date)
 
 # aggregate Timeseries to every three hours (mean)
 # !! careful, CRNS data with timestamp 09:00 descibres mean from 09:00-12:00, for this reason set right = F
 library(PaulsPack)
-test <- aggregate_by_time(ATHN,c(2:ncol(ATHN)),"3 hour",mean, right = F)
+ATHN <- aggregate_by_time(ATHN,c(2:ncol(ATHN)),"3 hour",mean, right = F)
+
+#aggregate_by_time changes type of date to "factor" and colname to "Group1. Should be fixed
+names(ATHN)[1] <- "Date"
+ATHN$Date <- as.POSIXct(ATHN$Date)
+
+# same length for both dataframes
+CR <- CR[CR$Date <= max(ATHN$Date)]
 
 ## moving window cross correlation
 
